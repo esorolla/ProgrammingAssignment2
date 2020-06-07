42953801c1b7fc812f1715b578c22f09b1e9db58
esorolla <eden.sorolla@gmail.com>
2020-06-07 12:22:47 +0200

    We come back to the initialization of the matrix with NULL within the function 'set' on line 11.
    We added comments to the different parts of the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,20 @@
 ## This function caches the calculation of a certain function on a
 ## matrix object.
 
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-    
+
+    ## We initialize the matrix object    
     m <- NULL
+    ## We set the matrix
     set <- function(y) {
         x <<- y
-        m <<- list()
+        m <<- NULL
     }
+    ## We get the matrix
     get <- function() x
+    ## We calculate the inverse of the matrix by using the function 'solve'
     setsolve <- function(solve) m <<- solve
+    ## We get the inverse of the function as a list
     getsolve <- function() m
     list(set = set, get = get,
          setsolve = setsolve,
@@ -20,17 +23,23 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## The next function calculates the inverse of the matrix x
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
+    ## We assign the calculation of the inverse of the matrix x to the "local"
+    ## variable m
     m <- x$getsolve()
-    if(!length(m)==0) {
+    if(!is.null(m)) { ## If the matrix x has not been created, we don't
+                      ## calculate the inverse, but we ouput the previously
+                      ## stored value.
         message("getting cached data")
         return(m)
     }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setsolve(m)
-    m
+    ## If the matrix x has been created, we output the inverse:
+    data <- x$get()       ## We create a local variable: 'data' with the created
+                          ## matrix x.
+    m <- solve(data, ...) ## Here we calculate the inverse of data within this
+                          ## environment.
+    x$setsolve(m) ## Here we calculate the inverse within the father environment
+                  ## (where x has been defined).
+    m             ## Here we print the inverse
 }
